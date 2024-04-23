0ea97132de72b9bce7507fb7207c9a5b7c099751
prince mumo <princeinkunzi@gmail.com>
2024-04-23 14:28:01 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,13 @@ function Home() {
     </div>
   );
 }
-
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 {/* write an <About> component here */}
 
 
@@ -23,7 +29,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
+      <Home />
+      <About />
       {/* add your <About> component here */}
     </div>
   );
